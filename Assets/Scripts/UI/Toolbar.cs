48d8fb20a31b0e37dafa9fd0f79b0dfa96f3c869
GareMetro <67924334+GareMetro@users.noreply.github.com>
2026-01-31 17:12:36 +0100

    Moins de hardcoding pour l'UI

--- Assets/Scripts/UI/Toolbar.cs
+++ Assets/Scripts/UI/Toolbar.cs
@@ -5,7 +5,9 @@ using UnityEngine.InputSystem.Controls;
 public class Toolbar : MonoBehaviour
 {
     [SerializeField] private GameObject SelectionPrefab;
-
+    [SerializeField] private float FirstToolPos; //Coordonnée X de l'outil 1 sur l'UI
+    [SerializeField] private float ToolSpacing; //Espace entre 2 outils sur l'UI
+    
     private RectTransform SelectionTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,11 +21,11 @@ public class Toolbar : MonoBehaviour
         if (toolIndex == 0)
         {
             //On a 8 outils donc ça arrive pas (c'est en dehors de l'écran btw)
-            SelectionTransform.localPosition = new Vector2(650, SelectionTransform.localPosition.y); //nice hardcoded position
+            SelectionTransform.localPosition = new Vector2(FirstToolPos + 10 * ToolSpacing, SelectionTransform.localPosition.y); //nice hardcoded position
         }
         else
         {
-            SelectionTransform.localPosition = new Vector2(-450 + 100 * toolIndex, SelectionTransform.localPosition.y); //nice hardcoded position
+            SelectionTransform.localPosition = new Vector2(FirstToolPos + ToolSpacing * (1 -toolIndex), SelectionTransform.localPosition.y); //nice hardcoded position
         }
     }
 }
