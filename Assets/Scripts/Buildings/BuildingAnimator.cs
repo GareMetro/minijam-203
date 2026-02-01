73c824a0bdd1313be56bcf10b9fd737af557bfd0
Aikeon <91202249+Aikeon@users.noreply.github.com>
2026-02-01 16:28:27 +0100

    Building animations Phase 3 END

--- Assets/Scripts/Buildings/BuildingAnimator.cs
+++ Assets/Scripts/Buildings/BuildingAnimator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 
@@ -67,94 +68,102 @@ public class BuildingAnimator : MonoBehaviour
                 launchSequence.Append(launcher.DORotate(new Vector3(-45f, 0f, 90f), 0.25f, RotateMode.Fast));
                 launchSequence.AppendInterval(0.2f);
                 launchSequence.Append(launcher.DORotate(new Vector3(-90f, 0f, 90f), 0.55f, RotateMode.Fast));
+                launchSequence.SetAutoKill(false);
                 break;
             case BuildingType.Cutter:    
                 hatchetRotation1 = DOTween.Sequence();
-                hatchetRotation1.Append(hatchet1.DORotate(new Vector3(-160f, 0f, -180f), 0.2f));
-                hatchetRotation1.Append(hatchet1.DORotate(new Vector3(-68f, 0f, -180f), 0.1f));
-                hatchetRotation1.Append(hatchet1.DORotate(new Vector3(-90f, 0f, -180f), 0.55f));
+                hatchetRotation1.Append(hatchet1.DORotate(new Vector3(-70f, 0f, 0f), 0.2f, RotateMode.WorldAxisAdd));
+                hatchetRotation1.Append(hatchet1.DORotate(new Vector3(92f, 0f, 0f), 0.15f, RotateMode.WorldAxisAdd));
+                hatchetRotation1.Append(hatchet1.DORotate(new Vector3(-22f, 0f, 0f), 0.55f, RotateMode.WorldAxisAdd));
+                hatchetRotation1.SetAutoKill(false);
                 
                 hatchetRotation2 = DOTween.Sequence();
                 hatchetRotation2.AppendInterval(0.075f);
-                hatchetRotation2.Append(hatchet1.DORotate(new Vector3(-160f, 0f, -180f), 0.2f));
-                hatchetRotation2.Append(hatchet1.DORotate(new Vector3(-68f, 0f, -180f), 0.1f));
-                hatchetRotation2.Append(hatchet1.DORotate(new Vector3(-90f, 0f, -180f), 0.55f));
+                hatchetRotation2.Append(hatchet2.DORotate(new Vector3(70f, 0f, 0f), 0.2f, RotateMode.WorldAxisAdd));
+                hatchetRotation2.Append(hatchet2.DORotate(new Vector3(-92f, 0f, 0f), 0.15f, RotateMode.WorldAxisAdd));
+                hatchetRotation2.Append(hatchet2.DORotate(new Vector3(22f, 0f, 0f), 0.55f, RotateMode.WorldAxisAdd));
+                hatchetRotation2.SetAutoKill(false);
                 
                 hatchetRotation3 = DOTween.Sequence();
                 hatchetRotation3.AppendInterval(0.15f);
-                hatchetRotation3.Append(hatchet1.DORotate(new Vector3(-160f, 0f, -180f), 0.2f));
-                hatchetRotation3.Append(hatchet1.DORotate(new Vector3(-68f, 0f, -180f), 0.1f));
-                hatchetRotation3.Append(hatchet1.DORotate(new Vector3(-90f, 0f, -180f), 0.55f));
+                hatchetRotation3.Append(hatchet3.DORotate(new Vector3(-70f, 0f, 0f), 0.2f, RotateMode.WorldAxisAdd));
+                hatchetRotation3.Append(hatchet3.DORotate(new Vector3(92f, 0f, 0f), 0.15f, RotateMode.WorldAxisAdd));
+                hatchetRotation3.Append(hatchet3.DORotate(new Vector3(-22f, 0f, 0f), 0.55f, RotateMode.WorldAxisAdd));
+                hatchetRotation3.SetAutoKill(false);
                 break;
             case BuildingType.Assembler:
                 //Arm 1
                 armBaseRotation1 = DOTween.Sequence();
-                armBaseRotation1.Append(armBase1.DORotate(new Vector3(-140f, -45f, 45f), 0.3f));
+                armBaseRotation1.AppendInterval(0.15f);
+                armBaseRotation1.Append(armBase1.DORotate(new Vector3(110f, 0f, 0f), 0.15f, RotateMode.LocalAxisAdd));
                 armBaseRotation1.AppendInterval(0.2f);
-                armBaseRotation1.Append(armBase1.DORotate(new Vector3(-90f, -45f, 45f), 0.3f));
+                armBaseRotation1.Append(armBase1.DORotate(new Vector3(-110f, 0f, 0f), 0.15f, RotateMode.LocalAxisAdd));
+                armBaseRotation1.SetAutoKill(false);
 
                 armExtendTranslation1 = DOTween.Sequence();
-                armExtendTranslation1.Append(armExtend1.DOLocalMove(new Vector3(-0.00431f,-0.00422f,0.00731f), 0.125f));
-                armExtendTranslation1.AppendInterval(0.05f);
-                armExtendTranslation1.Append(armExtend1.DOLocalMove(new Vector3(-0.004855f,-0.004855f,0.000655f), 0.125f));
-                armExtendTranslation1.AppendInterval(0.3f);
-                armExtendTranslation1.Append(armExtend1.DOLocalMove(new Vector3(-0.00431f,-0.00422f,0.00731f), 0.125f));
-                armExtendTranslation1.AppendInterval(0.05f);
-                armExtendTranslation1.Append(armExtend1.DOLocalMove(new Vector3(-0.004855f,-0.004855f,0.000655f), 0.125f));
+                armExtendTranslation1.Append(armExtend1.DOLocalMove(new Vector3(0f,0.61f,0.87f), 0.125f));
+                armExtendTranslation1.AppendInterval(0.5f);
+                armExtendTranslation1.Append(armExtend1.DOLocalMove(new Vector3(0f,0.6866f,0.0655f), 0.125f));
+                armExtendTranslation1.SetAutoKill(false);
 
                 //Arm 2
                 armBaseRotation2 = DOTween.Sequence();
                 armBaseRotation2.AppendInterval(0.05f);
-                armBaseRotation2.Append(armBase2.DORotate(new Vector3(-140f, 45f, -45f), 0.3f));
+                armBaseRotation2.AppendInterval(0.15f);
+                armBaseRotation2.Append(armBase2.DORotate(new Vector3(110f, 0f, 0f), 0.15f, RotateMode.LocalAxisAdd));
                 armBaseRotation2.AppendInterval(0.2f);
-                armBaseRotation2.Append(armBase2.DORotate(new Vector3(-90f, -45f, 45f), 0.3f));
+                armBaseRotation2.Append(armBase2.DORotate(new Vector3(-110f, 0f, 0f), 0.15f, RotateMode.LocalAxisAdd));
+                armBaseRotation2.SetAutoKill(false);
 
                 armExtendTranslation2 = DOTween.Sequence();
                 armExtendTranslation2.AppendInterval(0.05f);
-                armExtendTranslation2.Append(armExtend2.DOLocalMove(new Vector3(-0.00431f,-0.00422f,0.00731f), 0.125f));
-                armExtendTranslation2.AppendInterval(0.05f);
-                armExtendTranslation2.Append(armExtend2.DOLocalMove(new Vector3(-0.004855f,-0.004855f,0.000655f), 0.125f));
-                armExtendTranslation2.AppendInterval(0.3f);
-                armExtendTranslation2.Append(armExtend2.DOLocalMove(new Vector3(-0.00431f,-0.00422f,0.00731f), 0.125f));
-                armExtendTranslation2.AppendInterval(0.05f);
-                armExtendTranslation2.Append(armExtend2.DOLocalMove(new Vector3(-0.004855f,-0.004855f,0.000655f), 0.125f));
+                armExtendTranslation2.Append(armExtend2.DOLocalMove(new Vector3(0f,0.61f,0.87f), 0.125f));
+                armExtendTranslation2.AppendInterval(0.5f);
+                armExtendTranslation2.Append(armExtend2.DOLocalMove(new Vector3(0f,0.6866f,0.0655f), 0.125f));
+                armExtendTranslation2.SetAutoKill(false);
 
                 //Arm 3
                 armBaseRotation3 = DOTween.Sequence();
-                armBaseRotation2.AppendInterval(0.1f);
-                armBaseRotation3.Append(armBase3.DORotate(new Vector3(-140f, 45f, -45f), 0.3f));
+                armBaseRotation3.AppendInterval(0.1f);
+                armBaseRotation3.AppendInterval(0.15f);
+                armBaseRotation3.Append(armBase3.DORotate(new Vector3(110f, 0f, 0f), 0.15f, RotateMode.LocalAxisAdd));
                 armBaseRotation3.AppendInterval(0.2f);
-                armBaseRotation3.Append(armBase3.DORotate(new Vector3(-90f, -45f, 45f), 0.3f));
+                armBaseRotation3.Append(armBase3.DORotate(new Vector3(-110f, 0f, 0f), 0.15f, RotateMode.LocalAxisAdd));
+                armBaseRotation3.SetAutoKill(false);
 
                 armExtendTranslation3 = DOTween.Sequence();
-                armExtendTranslation2.AppendInterval(0.1f);
-                armExtendTranslation3.Append(armExtend3.DOLocalMove(new Vector3(-0.00431f,-0.00422f,0.00731f), 0.125f));
-                armExtendTranslation3.AppendInterval(0.05f);
-                armExtendTranslation3.Append(armExtend3.DOLocalMove(new Vector3(-0.004855f,-0.004855f,0.000655f), 0.125f));
-                armExtendTranslation3.AppendInterval(0.3f);
-                armExtendTranslation3.Append(armExtend3.DOLocalMove(new Vector3(-0.00431f,-0.00422f,0.00731f), 0.125f));
-                armExtendTranslation3.AppendInterval(0.05f);
-                armExtendTranslation3.Append(armExtend3.DOLocalMove(new Vector3(-0.004855f,-0.004855f,0.000655f), 0.125f));
+                armExtendTranslation3.AppendInterval(0.1f);
+                armExtendTranslation3.Append(armExtend3.DOLocalMove(new Vector3(0f,0.61f,0.87f), 0.125f));
+                armExtendTranslation3.AppendInterval(0.5f);
+                armExtendTranslation3.Append(armExtend3.DOLocalMove(new Vector3(0f,0.6866f,0.0655f), 0.125f));
+                armExtendTranslation3.SetAutoKill(false);
 
                 //Arm 4
                 armBaseRotation4 = DOTween.Sequence();
-                armBaseRotation2.AppendInterval(0.15f);
-                armBaseRotation4.Append(armBase4.DORotate(new Vector3(-140f, 45f, -45f), 0.3f));
+                armBaseRotation4.AppendInterval(0.15f);
+                armBaseRotation4.AppendInterval(0.15f);
+                armBaseRotation4.Append(armBase4.DORotate(new Vector3(110f, 0f, 0f), 0.15f, RotateMode.LocalAxisAdd));
                 armBaseRotation4.AppendInterval(0.2f);
-                armBaseRotation4.Append(armBase4.DORotate(new Vector3(-90f, -45f, 45f), 0.3f));
+                armBaseRotation4.Append(armBase4.DORotate(new Vector3(-110f, 0f, 0f), 0.15f, RotateMode.LocalAxisAdd));
+                armBaseRotation4.SetAutoKill(false);
 
                 armExtendTranslation4 = DOTween.Sequence();
-                armExtendTranslation2.AppendInterval(0.15f);
-                armExtendTranslation4.Append(armExtend4.DOLocalMove(new Vector3(-0.00431f,-0.00422f,0.00731f), 0.125f));
-                armExtendTranslation4.AppendInterval(0.05f);
-                armExtendTranslation4.Append(armExtend4.DOLocalMove(new Vector3(-0.004855f,-0.004855f,0.000655f), 0.125f));
-                armExtendTranslation4.AppendInterval(0.3f);
-                armExtendTranslation4.Append(armExtend4.DOLocalMove(new Vector3(-0.00431f,-0.00422f,0.00731f), 0.125f));
-                armExtendTranslation4.AppendInterval(0.05f);
-                armExtendTranslation4.Append(armExtend4.DOLocalMove(new Vector3(-0.004855f,-0.004855f,0.000655f), 0.125f));
+                armExtendTranslation4.AppendInterval(0.15f);
+                armExtendTranslation4.Append(armExtend4.DOLocalMove(new Vector3(0f,0.61f,0.87f), 0.125f));
+                armExtendTranslation4.AppendInterval(0.5f);
+                armExtendTranslation4.Append(armExtend4.DOLocalMove(new Vector3(0f,0.6866f,0.0655f), 0.125f));
+                armExtendTranslation4.SetAutoKill(false);
                 break;
             default: break;
         }
+        StartCoroutine(moncul());
+    }
+
+    IEnumerator moncul()
+    {
+        yield return new WaitForSeconds(3f);
+        PlayAnimation();
+        StartCoroutine(moncul());
     }
 
     public void PlayAnimation()
@@ -162,14 +171,14 @@ public class BuildingAnimator : MonoBehaviour
         switch (buildingType)
         {
             case BuildingType.Assembler:
-                armBaseRotation1.Play();
-                armExtendTranslation1.Play();
-                armBaseRotation2.Play();
-                armExtendTranslation2.Play();
-                armBaseRotation3.Play();
-                armExtendTranslation3.Play();
-                armBaseRotation4.Play();
-                armExtendTranslation4.Play();
+                armBaseRotation1.Restart();
+                armExtendTranslation1.Restart();
+                armBaseRotation2.Restart();
+                armExtendTranslation2.Restart();
+                armBaseRotation3.Restart();
+                armExtendTranslation3.Restart();
+                armBaseRotation4.Restart();
+                armExtendTranslation4.Restart();
                 break;
             case BuildingType.Cloner: 
                 sparks1.Play();
@@ -179,15 +188,15 @@ public class BuildingAnimator : MonoBehaviour
                 break;
             case BuildingType.Conveyor: break;
             case BuildingType.Cutter: 
-                hatchetRotation1.Play();
-                hatchetRotation2.Play();
-                hatchetRotation3.Play();
+                hatchetRotation1.Restart();
+                hatchetRotation2.Restart();
+                hatchetRotation3.Restart();
                 break;
             case BuildingType.Furnace: 
                 smoke.Play();
                 break;
             case BuildingType.Launcher:
-                launchSequence.Play();
+                launchSequence.Restart();
                 break;
             case BuildingType.Mixer:
                 spoonMover.MoveObject(spoon, 0.8f);
