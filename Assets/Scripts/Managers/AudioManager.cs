e4b1569fbbee64473ef85bd1e308b60ff6f361b3
Ugo L <ugo.larsonneur0@gmail.com>
2026-02-02 04:07:47 +0100

    fix audi

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -5,4 +5,19 @@ public class AudioManager : Singleton<AudioManager>
     public float generalVolume = 1;
     public float musicVolume = 1;
     public float sfxVolume = 1;
+
+    protected override void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+
+            if(TryGetComponent<AudioPlayer>(out AudioPlayer audio))
+                Destroy(audio);
+            return;
+        }
+        Instance = this;
+        if (DoNotDestroyOnLoad)
+            DontDestroyOnLoad(this);
+    }
 }
--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -5,7 +5,7 @@ public class Singleton<T> : MonoBehaviour where T : Singleton<T>
     public bool DoNotDestroyOnLoad = false;
     public static T Instance;
 
-    protected void Awake() 
+    protected virtual void Awake() 
     {
         if (Instance != null)
         {
